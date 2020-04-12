5e384c73b5955cc48c8c712ecb91f9320f93b627
eonsubae <eonsubae@naver.com>
2020-04-12 19:07:22 +0900

    Add graphql configuration

--- amplify-notetaker/src/index.js
+++ amplify-notetaker/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Amplify from 'aws-amplify';
+import aws_export from './aws-exports';
+
+Amplify.configure(aws_export);
 
 ReactDOM.render(
   <React.StrictMode>
