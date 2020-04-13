3ebdbc671fe27e0156f3886fb1c55b9ba9e94def
eonsubae <eonsubae@naver.com>
2020-04-13 19:01:50 +0900

    Add graphql code with AppSync

--- amplify-notetaker/src/App.js
+++ amplify-notetaker/src/App.js
@@ -1,22 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react';
 
-const defaultNotesValue = [
-  {
-    id: 1,
-    note: 'Hello World',
-  },
-];
+import { listNotes } from './graphql/queries';
+import { createNote } from './graphql/mutations';
 
-function App() {
-  const [notes, setNotes] = useState(defaultNotesValue);
+const App = (_) => {
+  const [note, setNote] = useState();
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    (async function getNotes() {
+      const result = await API.graphql(graphqlOperation(listNotes));
+      const newNotes = await result.data.listNotes.items;
+      setNotes(newNotes);
+    })();
+  }, []);
+
+  const handleChangeNote = (event) => setNote(event.target.value);
+
+  const handleAddNote = async (event) => {
+    event.preventDefault();
+    const input = { note };
+    const result = await API.graphql(graphqlOperation(createNote, { input }));
+    const newNote = result.data.createNote;
+    const updatedNotes = [newNote, ...notes];
+    setNotes(updatedNotes);
+    setNote('');
+  };
 
   return (
     <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
       <h1 className="code f2-1">Amplify Notetaker</h1>
       {/* Note Form */}
-      <form className="mb3">
-        <input type="text" className="pa2 f4" placeholder="Write your note" />
+      <form onSubmit={handleAddNote} className="mb3">
+        <input
+          type="text"
+          className="pa2 f4"
+          placeholder="Write your note"
+          onChange={handleChangeNote}
+          value={note}
+        />
         <button className="pa2 f4" type="submit">
           Add Note
         </button>
@@ -37,7 +61,7 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default withAuthenticator(App, {
   includeGreetings: true,
